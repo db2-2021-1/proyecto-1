1eea9fcd5890d1843f4a98310ba8422a57c46d20
otreblan <otreblain@gmail.com>
2021-05-19 12:41:09 -0500

    Add table class

---
+++ src/table.hpp
@@ -0,0 +1,53 @@
+// Proyecto 1
+// Copyright © 2021 otreblan
+//
+// proyecto-1 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proyecto-1 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proyecto-1.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include <utility>
+
+namespace db2
+{
+
+enum class index_type
+{
+	/// Extendible hash.
+	e_hash,
+
+	/// B+ tree.
+	bp_tree
+};
+
+/// This class manages the operations on a table.
+class table
+{
+private:
+	std::string name;
+
+	/// Name of the column used as primary key.
+	std::string primary_key;
+
+	index_type type;
+
+public:
+	table(std::string name):
+		name(std::move(name))
+	{};
+
+};
+
+};
