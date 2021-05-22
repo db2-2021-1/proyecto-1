b7274753424849a26b5189a93b2cde95cb60ab06
otreblan <otreblain@gmail.com>
2021-05-21 22:39:18 -0500

    Add base class for the SQL statements

---
+++ src/statement/statement.hpp
@@ -0,0 +1,37 @@
+// Proyecto 1
+// Copyright © 2021 otreblan
+//
+// proyecto-1 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proyecto-1 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proyecto-1.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <string>
+#include <utility>
+
+namespace db2::statement
+{
+
+class abstract
+{
+protected:
+	std::string table_name;
+
+	abstract(std::string table_name):
+		table_name(std::move(table_name))
+	{};
+
+	virtual bool execute() = 0;
+};
+
+}
